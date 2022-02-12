61f61345b27fe0552050aabc2df0cce3c60401cf
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-12 17:33:01 +0530

    Creating a makecampground model in routes folder

---
+++ routes/makeCampground.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const express = require('express');
+//@ Importing campgroundSchema model
+const camp = require('/Users/Personal/Desktop/YelpCamp/model/campground'); 
+const res = require("express/lib/response");
+const route=express.Router();
+
+
+route.get('/makecampground',async (req,res)=>{
+    await 
+})
+
+
