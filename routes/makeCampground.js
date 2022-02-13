890fd7ae5d4e7d91695d6e41ba004576995a059f
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-14 02:11:44 +0530

    made some little changes

--- routes/makeCampground.js
+++ routes/makeCampground.js
@@ -9,31 +9,41 @@ const route=express.Router();
 
 
 route.get('/',async (req,res)=>{
-    const allPost=await Campground.find();
-    res.json(allPost);
-})
-
-route.get('/createpost',async (req,res)=>{
-    const samplePost = await new Campground({
-      title: "Wayanad",
-      price: "$30",
-      description:"Good Place"
-    });
-    samplePost.save().then(() => {
-        console.log("Successfully saved");
-        res.json(samplePost);
-    }).catch((err) => {
-        console.log(new Error(err).message, res.statusCode);
-    });
+    res.render("home");
 })
 
 
-route.delete('/:postId',async (req,res)=>{
-    const {postId}=req.params;
-    console.log(postId);
-    const delPost=await Campground.findByIdAndDelete(postId);
-    res.json(delPost);
-})
+
+
+
+
+
+
+
+
+
+
+// route.get('/createpost',async (req,res)=>{
+//     const samplePost = await new Campground({
+//       title: "Wayanad",
+//       price: "$30",
+//       description:"Good Place"
+//     });
+//     samplePost.save().then(() => {
+//         console.log("Successfully saved");
+//         res.json(samplePost);
+//     }).catch((err) => {
+//         console.log(new Error(err).message, res.statusCode);
+//     });
+// })
+
+
+// route.delete('/:postId',async (req,res)=>{
+//     const {postId}=req.params;
+//     console.log(postId);
+//     const delPost=await Campground.findByIdAndDelete(postId);
+//     res.json(delPost);
+// })
 
 
 module.exports=route;
