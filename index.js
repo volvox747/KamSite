e0cb5a180714861ee37148979b665f8e6a018e87
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-14 11:30:33 +0530

    Made some changes

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
+const app = express();
+
+
+
+
 //@ Connecting to MongoDB 
 mongoose.connect("mongodb://localhost:27017/kam-site").then(() => {
   console.log("Connected to MongoDB");
 }).catch(e=>{new Error(e).message})
 
-const app = express();
 
-//@ setting the views and view engine 
+//@ Setting the views and view engine 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,35 +2,51 @@
 const mongoose = require("mongoose");
 
 //@ Importing data from seeds folder
-const cities=require("../seeds/cities");
-const {descriptors,places}=require("../seeds/seedHelpers");
+const cities = require("../seeds/cities");
+const {
+    descriptors,
+    places
+} = require("../seeds/seedHelpers");
 
 //!       (or in other ways)
 // const descriptors=require("../seeds/seedHelpers").descriptors; 
 // const places=require("../seeds/seedHelpers").places 
 
+//@ Connecting to Database 
+mongoose
+    .connect("mongodb://localhost:27017/kam-site")
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((e) => {
+        new Error(e).message;
+    });
 
-//@ Importing Model
+
+    //@ Importing Model
 const Campground = require("../model/campground");
 
-const dataBase= async ()=>{
+//@ Delete the existing contents from Database
+//@ Insert new data into the database from dummy data   
+const dataBase = async () => {
     await Campground.deleteMany({});
 
-    for(let i=0;i<=300;i++)
-    {
-        const random1000=Math.floor(Math.random()*1000);
-        const campground=new Campground({title:cities[random1000]},location:`${sampleLocation()}`)
+    for (let i = 0; i <= 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const campground = new Campground({
+          title:
+            descriptors[Math.floor(Math.random() * descriptors.length)] +
+            " " +
+            places[Math.floor(Math.random() * places.length)],
+          location: cities[random1000].city,
+        });
+
+        await campground.save();
     }
 }
 
 
+dataBase().then(()=>mongoose.connection.close());
 
 
-mongoose
-  .connect("mongodb://localhost:27017/kam-site")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((e) => {
-    new Error(e).message;
-  });
+
