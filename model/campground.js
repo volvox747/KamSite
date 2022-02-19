80b4e1534c888c37b95964dd43616413b23e0d37
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-19 18:09:38 +0530

    Added required option to the schema model

--- model/campground.js
+++ model/campground.js
@@ -3,11 +3,26 @@ const mongoose = require('mongoose');
 const Schema=mongoose.Schema;
 
 const campgroundSchema=new Schema({
-    title:String,
-    price:Number,
-    description:String,
-    location:String,
-    image:String
+    title:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
 });
 
 module.exports=mongoose.model('Campground',campgroundSchema);
