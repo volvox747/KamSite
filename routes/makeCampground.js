cf6682eedcf94ce3aed4352f9ae59c2dd9711f58
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-20 17:06:00 +0530

    Imported Joi module and Defined a serverSide validation function to validate and handle the errors which occur in server side

--- routes/makeCampground.js
+++ routes/makeCampground.js
@@ -1,76 +1,118 @@
-const mongoose = require("mongoose");
 const express = require('express');
 
+//@ Importing a validation tool 'JOI' which is easy when compared to writing custome or manually validations
+//@ This tool was introduced as it is easy to validate rather than writing custom validations
+const Joi = require('joi');
+
+//@ Importing validationSchema to validate the data or server side validation  
+const {validationSchema}=require('../schemas');
+
+
 //@ Importing common Async-Error handling wrapper function to handle async errors and Custom Error class  
-const catchAsync=require('../utils/catchAsyncError');
-const ExpressError=require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsyncError');
+const ExpressError = require('../utils/ExpressError');
 
 //@ Importing campgroundSchema model
-const Campground = require('../model/campground'); 
-const route=express.Router();
+const Campground = require('../model/campground');
+const { append } = require('express/lib/response');
+const route = express.Router();
 
 
-//@ CREATE
+const validationFunction= (req,res,next)=>{
+  //@ validating the incoming data using the above defined model
+  const validatedResult = validationSchema.validate(req.body);
 
-route.get('/newcampground',catchAsync((req,res)=>{
+  //@ destructuring the "error" object to handle any server side errors
+  const { error } = validatedResult;
+
+  //@ if any error take the "details"(array of objs) value inside the error object
+  if (error) 
+  {
+    //@ using "map" to map the object's msg from details
+    //@ using "join" to join, if there are more than 1 msg
+    const msg = error.details.map((err) => err.message).join(",");
+    throw new ExpressError(msg, 400);
+  }
+  next();
+}
+
+//$ CREATE
+
+route.get('/newcampground', catchAsync((req, res) => {
     res.render('newCampground');
 }))
 
-route.post('/newcampground/create',catchAsync(async (req,res)=>{
-        const newCampGround = new Campground({
-            title: req.body.title,
-            location: req.body.location,
-            price: req.body.price,
-            image: req.body.image,
-            description: req.body.description,
-        });
-        await newCampGround.save();
-        res.redirect(`/show/${newCampGround._id}`);
+//@ validationFunction middleware is used so that server side validation is done before saving to the database 
+route.post('/newcampground/create', validationFunction, catchAsync(async (req, res) => {
     
-}))
-
-
-//@ READ 
-route.get('/find',catchAsync(async (req,res)=>{
-    const campgrounds=await Campground.find({});
-    res.render("home",{campgrounds});
-}))
-
-route.get('/show/:id',catchAsync(async (req,res)=>{
-    const {id}=req.params;
-    const showCampGround=await Campground.findById(id);
-    res.render("showCampground",{showCampGround});
-}))
-
-
-//@ UPDATE 
-
-route.get('/editcampground/:id/edit',catchAsync(async (req,res)=>{
-    const {id}=req.params;
-    const editCampGround=await Campground.findById(id);
-    res.render('editCampground',{editCampGround})
-}))
-
-route.put('/editcampground/:id',catchAsync(async (req,res)=>{
-    const {id}=req.params;
-    await Campground.findByIdAndUpdate(
-      id,
-      {
+    const newCampGround = new Campground({
         title: req.body.title,
         location: req.body.location,
         price: req.body.price,
         image: req.body.image,
         description: req.body.description,
-      }
+    });
+    await newCampGround.save();
+    res.redirect(`/show/${newCampGround._id}`);
+
+}))
+
+
+//$ READ 
+route.get('/find', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("home", {
+        campgrounds
+    });
+}))
+
+route.get('/show/:id', catchAsync(async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const showCampGround = await Campground.findById(id);
+    res.render("showCampground", {
+        showCampGround
+    });
+}))
+
+
+//$ UPDATE 
+
+route.get('/editcampground/:id/edit', catchAsync(async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const editCampGround = await Campground.findById(id);
+    res.render('editCampground', {
+        editCampGround
+    })
+}))
+
+//@ validationFunction middleware is used so that server side validation is done before saving to the database 
+route.put('/editcampground/:id', validationFunction, catchAsync(async (req, res) => {
+    const {
+        id
+    } = req.params;
+    await Campground.findByIdAndUpdate(
+        id, {
+        title: req.body.title,
+        location: req.body.location,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description,
+    }
     );
     res.redirect(`/show/${id}`);
 }))
 
 
-//@ DELETE
+//$ DELETE
 
-route.delete('/editcampground/:id',catchAsync(async (req,res)=>{
-    const {id}=req.params;
+route.delete('/editcampground/:id', catchAsync(async (req, res) => {
+    const {
+        id
+    } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/find');
 }))
@@ -78,23 +120,23 @@ route.delete('/editcampground/:id',catchAsync(async (req,res)=>{
 
 
 //@ This route handles express errors whenever the routes are invalid or does'nt match
-route.all('*',catchAsync(async (req,res,next)=>{
-    throw new ExpressError('Not Found!!',404);
+route.all('*', catchAsync(async (req, res, next) => {
+    throw new ExpressError('Not Found!!', 404);
 }))
 
 
-//? Error handling middleware 
-route.use((err,req,res,next)=>{
-    //& Destructuring and setting default values for destructured elements 
-    if(!err.statusCode) err.statusCode=500;
-    else if(!err.message) err.message='Something Wrong';
-    res.render('error',{err});
+//$ Error handling middleware 
+route.use((err, req, res, next) => {
+    //@ Setting default values if err.status and err.msg is not specified by default
+    if (!err.statusCode) err.statusCode = 500;
+    if (!err.message) err.message = 'Something Wrong';
+    res.render('error', {
+        err
+    });
 })
 
 
 
 
 
-module.exports=route;
-
-
+module.exports = route;
