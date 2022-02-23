792e4eacde32b277f3375f282a15821f4c030d64
Benson Thomas <bensonthomascr7@gmail.com>
2022-02-23 23:30:55 +0530

    Used a mongoose middleware "findOneAndDelete" which deletes all the reviews of a particular deleted campground

--- model/campground.js
+++ model/campground.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const review = require('./review');
+const Review = require('./review');
 
 const Schema=mongoose.Schema;
 
@@ -32,4 +32,10 @@ const campgroundSchema=new Schema({
     ]
 });
 
+campgroundSchema.post('findOneAndDelete',async (campground)=>{
+    if(campground.reviews.length!=0)
+    {
+        await Review.deleteMany(_id,{$in:campground.reviews});
+    }
+})
 module.exports=mongoose.model('Campground',campgroundSchema);
