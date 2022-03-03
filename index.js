26714cd3778c18560219c06bb20d129b027ef395
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-03 16:56:50 +0530

    Configure passport.js

--- index.js
+++ index.js
@@ -26,6 +26,31 @@ app.use(session({
 })); 
 
 
+
+//@ Importing passport-local-mongoose and passport
+  //` authentication middleware for node.js
+const passport = require('passport');
+  //` LocalStrategy is one of the strategies used for login purpose, 
+  //` It authenticates manually typed username and password(local login) 
+const LocalStrategy = require("passport-local");
+
+//@ To use Passport in an Express or Connect-based application, configure it with the required passport.initialize() middleware
+app.use(passport.initialize());
+
+//@ To make the user logged-in unless he manually logs out (in other words persistent login sessions) use passport.session() middleware
+app.use(passport.session());
+
+//@ Importing User model and authenticating the user using local strategy
+const User=require('./model/user')
+passport.use(new LocalStrategy(User.authenticate()));
+//@ add the user to the session data
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
 //@ Importing m'method-override' pck to do additional request such as PUT,PATCH,DELETE 
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
