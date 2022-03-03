73dc42d7efec02cb01ddc15f5b4a1b5112964109
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-03 20:35:55 +0530

    Added get and post routes of login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const route = express.Router({ mergeParams: true });
 
 
+const catchAsync = require("../utils/catchAsyncError");
+
 //@ Importing User Model
 const User = require('../model/user');
 
@@ -10,6 +12,20 @@ route.get('/register',async(req,res)=>{
     res.render('register');
 })
 
-
+route.post('/register',catchAsync(async(req,res)=>{
+    try{
+        const {username,email,password}=req.body;
+        const user=new User({username,email});
+        const ans=await User.register(user,password);
+        console.log(ans)
+        req.flash('success',"Welcome to KamSite")
+        res.redirect('/campground/find');
+    }
+    catch(err)
+    {
+        req.flash('error',err.message);
+        res.redirect('/register')
+    }
+}))
 
 module.exports=route;
