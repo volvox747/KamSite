689e9efb27b3a4a53826733644de8ccde883867f
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-03 17:17:32 +0530

    Added a get route to register

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const route = express.Router({ mergeParams: true });
+
+
+//@ Importing User Model
+const User = require('../model/user');
+
+
+route.get('/register',async(req,res)=>{
+    res.render('register');
+})
+
