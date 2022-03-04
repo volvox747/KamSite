566f92a72066e3b19db54ea7fc3b4ace2fe44cc8
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-04 14:05:10 +0530

    Added isAuthor middleware to authorize before accessing edit page or deleting a campground

--- middleware.js
+++ middleware.js
@@ -1,8 +1,32 @@
-module.exports.isLoggedIn = (req, res, next) => {
+//@ Importing Campground Model
+const Campground = require("./model/campground");
+
+
+
+
+
+//* To check whether the user is loggedIn before accessing newcamground page
+
+const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.returnTo=req.originalUrl;
+    // checks whether user is authenticated or not
+    req.session.returnTo = req.originalUrl; // returns the current url where the the process was interrupted
     req.flash("error", "You are not logged in");
     return res.redirect("/login"); // if you dont use "return" it will cause can't set header after they are send to client
   }
   next();
 };
+
+//* To check if someone access the edit,delete route through url
+const isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to update");
+    return res.redirect(`/campground/show/${id}`);
+  }
+  next();
+};
+
+
+module.exports={isLoggedIn,isAuthor};
