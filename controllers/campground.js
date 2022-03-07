a6c55a53e82ee551079a8fa29e16e921050e5827
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-08 01:10:24 +0530

    Added coordinates into the campground model/collection

--- controllers/campground.js
+++ controllers/campground.js
@@ -2,6 +2,12 @@
 //@ Importing Campground Model
 const Campground = require("../model/campground");
 
+const mapBoxToken=process.env.MAPBOX_TOKEN;
+
+//@ Importing Geocoding mapbox services 
+const mbxGeoCoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocoder=mbxGeoCoding({accessToken:mapBoxToken}) //contains frwd and reverse geocoding
+
 
 const showNewCampGround = (req, res) => {
 res.render("newCampground");
@@ -11,6 +17,11 @@ res.render("newCampground");
 
 const postNewCampGround = async (req, res) => 
 {
+    const geoData=await geocoder.forwardGeocode({
+        query:req.body.location,// this takes the location string and gives the geocoding of it
+        limit:1
+    }).send();
+
     //^ adds new campground data entered by user into the campground collection in kam-site database 
     const newCampGround = new Campground({
         title: req.body.title,
@@ -18,11 +29,13 @@ const postNewCampGround = async (req, res) =>
         price: req.body.price,
         images: req.files.map(file=>({url:file.path,filename:file.filename})), //^ stores the file url and filename of uploaded files
         description: req.body.description,
+        geometry:geoData.body.features[0].geometry
     });
     //^ adds the user who created the campground 
     let ans = req.user._id.toString(); //^ req.user._id will be in new ObjectId('5345363c453453') format which cannot be inserted into mongoDB
     newCampGround.author = ans; //^ this registers the id of the user which is created on registering the website
     await newCampGround.save(); //^ saves the data into camground collection 
+    console.log(newCampGround);
     req.flash("success", "Successfully created a new campground!!"); //^ displays a success msg after creating a campground
     res.redirect(`/campground/show/${newCampGround._id}`); //^ redirects the control to showcamground page
 }
