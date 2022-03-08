33cf63427a929f654bd595028707593da57e571d
Benson Thomas <bensonthomascr7@gmail.com>
2022-03-09 00:50:50 +0530

    Added marker and transported location coordinates to point the marker to desired location

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,12 @@
 mapboxgl.accessToken = mapToken;
+console.log(campground);
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom:9,
+    center: campground.geometry.coordinates
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
